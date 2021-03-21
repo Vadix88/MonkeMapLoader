52ec40e8fc3966a13d9585ace7f39dbe6f558dbe
Toni Macaroni <tonimacaroniy@gmail.com>
2021-03-21 02:44:36 +0100

    update dependency

--- MonkeMapLoader/MonkeMapLoaderPlugin.cs
+++ MonkeMapLoader/MonkeMapLoaderPlugin.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 namespace VmodMonkeMapLoader
 {
     [BepInPlugin("vadix.gorillatag.maploader", "Monke Map Loader", "1.0.0")]
-    [BepInDependency("tonimacaroni.computerinterface", "1.3.1")]
+    [BepInDependency("tonimacaroni.computerinterface", "1.4.0")]
     [BepInDependency("org.legoandmars.gorillatag.utilla", "1.3.0")]
     public class MonkeMapLoaderPlugin : BaseUnityPlugin
     {
