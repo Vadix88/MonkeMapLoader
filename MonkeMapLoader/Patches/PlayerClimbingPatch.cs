ec05b7f8ff95e016875fac2d811c8af7c2eb47e6
Graicc <33105645+Graicc@users.noreply.github.com>
2021-04-01 03:56:02 -0400

    Fix for updated locomotion

--- MonkeMapLoader/Patches/PlayerClimbingPatch.cs
+++ MonkeMapLoader/Patches/PlayerClimbingPatch.cs
@@ -1,49 +1,37 @@
 ﻿using GorillaLocomotion;
 using HarmonyLib;
 using UnityEngine;
-using VmodMonkeMapLoader.Behaviours;
 
 namespace VmodMonkeMapLoader.Patches
 {
-    [HarmonyPatch(typeof(Player))]
-    [HarmonyPatch("CollisionsSphereCast", MethodType.Normal)]
-    internal class PlayerClimbingPatch
-    {
-        public static bool disabledBecauseUnclimbable = false;
-        internal static void Postfix(Player __instance, ref bool __result, float sphereRadius, RaycastHit hitInfo)
-        {
-            if (__result)
-            {
-                if (sphereRadius == __instance.minimumRaycastDistance)
-                {
-                    // it's a hand, do stuff.
-                    SurfaceClimbSettings settings = hitInfo.collider.gameObject.GetComponent<SurfaceClimbSettings>();
-                    //if (settings != null && settings.Unclimbable == true) __result = false;
-                    if (settings != null && settings.Unclimbable == true)
-                    {
-                        // UNCLIMBABLE. DAS BAD.
-                        if (!__instance.disableMovement)
-                        {
-                            __instance.disableMovement = true;
-                            disabledBecauseUnclimbable = true;
-                        }
-                    }
-                }
-            }
-        }
-    }
+	[HarmonyPatch(typeof(Player))]
+	[HarmonyPatch("GetSlidePercentage", MethodType.Normal)]
+	internal class PlayerClimbingPatch
+	{
+		internal static bool Prefix(Player __instance, ref float __result, RaycastHit raycastHit)
+		{
+			// Check for surface component
+			Surface surface = raycastHit.collider.GetComponent<Surface>();
+			if (surface != null)
+			{
+				__result = surface.slipPercentage;
+				return false;
+			}
 
-    [HarmonyPatch(typeof(Player))]
-    [HarmonyPatch("Update", MethodType.Normal)]
-    internal class PlayerClimbingMovementResetPatch
-    {
-        internal static void Postfix(Player __instance)
-        {
-            if(__instance.disableMovement && PlayerClimbingPatch.disabledBecauseUnclimbable)
-            {
-                __instance.disableMovement = false;
-                PlayerClimbingPatch.disabledBecauseUnclimbable = false;
-            }
-        }
-    }
+			// Check for unreadable mesh
+			MeshCollider meshCollider = raycastHit.collider as MeshCollider;
+			if (meshCollider == null || meshCollider.sharedMesh == null)
+			{
+				__result = __instance.defaultSlideFactor;
+				return false;
+			}
+			if (!meshCollider.sharedMesh.isReadable)
+			{
+				__result = __instance.defaultSlideFactor;
+				return false;
+			}
+
+			return true;
+		}
+	}
 }
