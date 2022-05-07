bcda84ece3a23f85beddaa23b03e308eba1f387d
Graicc <33105645+Graicc@users.noreply.github.com>
2022-05-07 01:20:36 -0400

    Fix build script

--- MonkeMapLoader/MakeRelease.ps1
+++ MonkeMapLoader/MakeRelease.ps1
@@ -1,9 +1,15 @@
-$Name = "MonkeMapLoader" # Replace with your mods name
+# https://gist.github.com/Graicc/ad26b80ac1a733edc254053fdf012ffe
+# Needs to be at least that version, or mmm can't read the archive
+#Requires -Modules @{ ModuleName="Microsoft.PowerShell.Archive"; ModuleVersion="1.2.3" }
+$MyInvocation.MyCommand.Path | Split-Path | Push-Location # Run from this script's directory
+$Name = (ls *.csproj).BaseName
+dotnet build -c Release
+# mkdir BepInEx\plugins\$Name
 curl -L https://github.com/Vadix88/MonkeMapLoader/releases/download/1.1.2/MonkeMapLoader-1.1.2.zip -o DL.zip
 Expand-Archive DL.zip 
 rm DL.zip
-dotnet build -c Release -o Temp
-cp Temp\$Name.dll DL\BepInEx\plugins\$Name\$Name.dll
-rmdir Temp -Recurse
-Compress-Archive DL\BepInEx\ $Name-v.zip 
-rmdir DL -Recurse
+mv DL\BepInEx .
+cp bin\Release\netstandard2.0\$Name.dll BepInEx\plugins\$Name\
+Compress-Archive .\BepInEx\ $Name-v
+rmdir .\BepInEx\ -Recurse
+Pop-Location
